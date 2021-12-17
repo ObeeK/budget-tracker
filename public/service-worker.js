af9f167474edebbcf8bdf09b16d861df38e4491e
ObeeK <chasenharvey@gmail.com>
2021-12-17 15:03:54 -0600

    working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,26 @@
-const CACHE_NAME = 'my-site-cache-v2';
-const DATA_CACHE_NAME = 'data-cache-v2';
+const CACHE_NAME = 'my-site-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/js/idb.js',
-  '/js/index.js',
-  '/css/style.css',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-348x348.png',
-  '/icons/icon-512x512.png',
+  './index.html',
+  './manifest.json',
+  './js/idb.js',
+  './js/index.js',
+  './css/styles.css',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
 ];
 
 // Install the service worker
 self.addEventListener('install', function(evt) {
   evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then(function (cache) {
       console.log('Your files were pre-cached successfully!');
       return cache.addAll(FILES_TO_CACHE);
     })
